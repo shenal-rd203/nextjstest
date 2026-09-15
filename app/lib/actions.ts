a5c8f671449d7bd30f5f149c28e32e5b939dc1f6
Shenal Rayantha <101634665+shenal-rd203@users.noreply.github.com>
2026-09-15 21:41:18 +0530

    chapter 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,15 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
     
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+      await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+        console.error('Error inserting invoice:', error);
+        throw new Error('Failed to create invoice');
+    }
 
     revalidatePath('/dashboard/invoices');
       redirect('/dashboard/invoices');
